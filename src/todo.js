c76d26cf10e939ed0dbce779d02e8cc6734b85a3
abdu djuraev <abdudj575@gmail.com>
2026-04-06 15:14:56 -0400

    todos-16

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,28 @@
 class Todo {
+    #id;
+    #text;
+    #completed;
+
     constructor(id, text, completed = false){
-        this.id = id;
-        this.text = text;
-        this.completed = completed;
+        this.#id = id;
+        this.#text = text;
+        this.#completed = completed;
     }
 
     toggle(){
-        this.completed = !this.completed;
+        this.#completed = !this.#completed;
+    }
+
+    get id(){
+        return this.#id;
+    }
+
+    get text(){
+        return this.#text;
+    }
+
+    get completed(){
+        return this.#completed;
     }
 }
 
@@ -14,5 +30,5 @@ const todo = new Todo(1, "Buy milk", false);
 console.log(todo.id);
 console.log(todo.text);
 console.log(todo.completed);
-todo.id = 2;
-console.log(todo);
+todo.toggle();
+console.log(todo.completed);
